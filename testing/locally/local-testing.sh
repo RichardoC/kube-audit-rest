c81a89cc05c4eee943e16bc9c5f472935859736f
Richard Tweed <RichardoC@users.noreply.github.com>
2022-12-02 21:18:14 +0000

    Add exmaple workflow and add some testing to docker image building

--- testing/locally/local-testing.sh
+++ testing/locally/local-testing.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-# set -eu
+set -euo pipefail
 
 ROOT=$(git rev-parse --show-toplevel)
 
 cd $ROOT
 
+# Ensure tmp folder exists
+mkdir -p tmp/
+
 # Ensure local certs exist
 testing/certs.sh
+
 
 echo "Removing old test file"
 rm -f tmp/kube-audit-rest.log;
 
 echo "Removing old servers if still running"
-pkill kube-audit-rest
+pkill kube-audit-rest || echo "No old server running"
 
 # Run current server with those local certs on port 9090
 go run . --cert-filename=./tmp/server.crt --cert-key-filename=./tmp/server.key --server-port=9090 --logger-filename=./tmp/kube-audit-rest.log &
@@ -29,4 +33,4 @@ sleep 2 # Scientific way of waiting for the file to be written as async...
 # Removing backgrounded process
 pkill kube-audit-rest
 
-diff testing/locally/data/kube-audit-rest.log tmp/kube-audit-rest.log && [ "$TEST_EXIT" -eq "0" ]&& echo "Test passed" || echo "output not as expected"
+diff testing/locally/data/kube-audit-rest.log tmp/kube-audit-rest.log && [ "$TEST_EXIT" -eq "0" ] && echo "Test passed" || bash -c 'echo "output not as expected" && exit 255'
