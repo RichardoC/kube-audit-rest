1dec90d4068d2c8fd9a81c3d99c925864ca6c641
Jordi Pique <jordipiqueselles@gmail.com>
2023-05-11 20:31:54 +0100

    In the prometheus tests, retry the requests if the server is not running yet

--- internal/metrics/prometheus_metrics/prometheus_metrics_test.go
+++ internal/metrics/prometheus_metrics/prometheus_metrics_test.go
@@ -14,6 +14,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var REQUEST_MAX_RETRIES int = 10
+
 func getFreePort() int {
 	listener, err := net.Listen("tcp", ":0")
 	if err != nil {
@@ -39,18 +41,24 @@ func Test_WhenServerStarted_ThenServesRequests(t *testing.T) {
 	port := getFreePort()
 	ms := prometheusmetrics.New(port)
 	go ms.Start()
-	// Wait 100ms so the Prometheus server will be ready to receive the request that
-	// we'll do in the next lines
-	time.Sleep(100 * time.Millisecond)
 	counter := ms.CreateAndRegisterCounter("test_counter", "This counter is for test purposes")
 	counter.Inc()
 
-	// Make request and check we get an answer
+	// Repeat the request up to 10 times
+	// We need that since we may start doing the request before the server is fully up and running
 	requestURL := fmt.Sprintf("http://localhost:%d/metrics", port)
 	res, err := http.Get(requestURL)
+	for i := 0; i < REQUEST_MAX_RETRIES; i++ {
+		if err == nil {
+			break
+		}
+		time.Sleep(100 * time.Millisecond)
+		res, err = http.Get(requestURL)
+	}
 	if err != nil {
 		log.Fatalf("Error making http request: %s\n", err)
 	}
+
 	// Read http body
 	defer res.Body.Close()
 	body, err := io.ReadAll(res.Body)
