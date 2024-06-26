eb8791e21e51b60999558bfb868d7830bdd19134
Lukas Neuenschwander <lukas.neu24@gmail.com>
2024-06-26 19:58:51 +0200

    Fix full elastic stack example (#173)
    * fixed certs.sh with "" around the paths
    
    This ensures the script can be run anywhere. Turns out I checked out the
    repository in a folder with spaces in some level above and the script
    did not run. With this fix in place, the script runs nicely anywhere
    
    * add resource limits and requests to vector
    
    VS Code complained about the vector container not having resource limits
    defined. These limits have been tested to work in my microk8s deployment
    with quite some other stuff than this stack running, and it worked fine

--- examples/full-elastic-stack/certs/certs.sh
+++ examples/full-elastic-stack/certs/certs.sh
@@ -4,20 +4,19 @@ set -eux
 
 # Default to root if no .git missing
 ROOT=$(git rev-parse --show-toplevel || echo '.' )
+cd "$ROOT"
 
-cd $ROOT
+CONF_DIR="$(pwd)/examples/full-elastic-stack/certs"
 
-CONF_DIR=$(pwd)/examples/full-elastic-stack/certs
-
-TMP=$(pwd)/tmp/full-elastic-stack
-echo $TMP
-mkdir -p $TMP
-cd $TMP
+TMP="$(pwd)/tmp/full-elastic-stack"
+echo "$TMP"
+mkdir -p "$TMP"
+cd "$TMP"
 
 openssl genrsa -out rootCA.key 2048 &> /dev/null
 
-openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 1460 -out rootCA.pem -config $CONF_DIR/ca.cnf &> /dev/null
+openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 1460 -out rootCA.pem -config "$CONF_DIR/ca.cnf" &> /dev/null
 
-openssl req -new -nodes -sha256 -out server.csr -newkey rsa:2048 -keyout server.key -config $CONF_DIR/server.csr.cnf &> /dev/null
+openssl req -new -nodes -sha256 -out server.csr -newkey rsa:2048 -keyout server.key -config "$CONF_DIR/server.csr.cnf" &> /dev/null
 
-openssl x509 -req -in server.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out server.crt -days 500 -sha256 -extensions v3_req -extfile $CONF_DIR/server.csr.cnf &> /dev/null
+openssl x509 -req -in server.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out server.crt -days 500 -sha256 -extensions v3_req -extfile "$CONF_DIR/server.csr.cnf" &> /dev/null
